2d0d9d8f33874c4cdc24d021610a1ab010334abf
Paulus Gandung <gandung@slackerc0de.us>
2020-02-23 17:45:30 +0700

    Adding src/Pass/RouterFactoryPass.php

---
+++ src/Pass/RouterFactoryPass.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LilleBitte\Extension\FrameworkExtension\Pass;
+
+use LilleBitte\Container\ContainerBuilderInterface;
+use LilleBitte\Container\CompilerPassInterface;
+use LilleBitte\Routing\RouterFactory;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class RouterFactoryPass implements CompilerPassInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __invoke(ContainerBuilderInterface $container)
+	{
+		$container->instance($this->getTag(), RouterFactory::getRouter());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTag()
+	{
+		return 'extension.framework.router_factory';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getAssociatedClass()
+	{
+		return RouterFactory::class;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getSerializedValue()
+	{
+		return 'RouterFactory::getRouter()';
+	}
+}
