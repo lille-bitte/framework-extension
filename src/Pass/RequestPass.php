fbb64bb902a90c8c1a481d134422f6bdb779188c
Paulus Gandung <gandung@slackerc0de.us>
2020-02-23 17:43:37 +0700

    Adding src/Pass/RequestPass.php

---
+++ src/Pass/RequestPass.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LilleBitte\Extension\FrameworkExtension\Pass;
+
+use LilleBitte\Container\ContainerBuilderInterface;
+use LilleBitte\Container\CompilerPassInterface;
+use LilleBitte\Messenger\Request;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class RequestPass implements CompilerPassInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __invoke(ContainerBuilderInterface $container)
+	{
+		$container->instance($this->getTag(), new Request());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTag()
+	{
+		return 'extension.framework.request';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getAssociatedClass()
+	{
+		return Request::class;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getSerializedValue()
+	{
+		return 'new Request()';
+	}
+}
