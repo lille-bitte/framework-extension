7b7b9b3dba9aaf34a6bffb6fcda3aca0facf99d6
Paulus Gandung <gandung@slackerc0de.us>
2020-02-23 17:44:03 +0700

    Adding src/Pass/ResponsePass.php

---
+++ src/Pass/ResponsePass.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LilleBitte\Extension\FrameworkExtension\Pass;
+
+use LilleBitte\Container\ContainerBuilderInterface;
+use LilleBitte\Container\CompilerPassInterface;
+use LilleBitte\Messenger\Response;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class ResponsePass implements CompilerPassInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __invoke(ContainerBuilderInterface $container)
+	{
+		$container->instance($this->getTag(), new Response());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTag()
+	{
+		return 'extension.framework.response';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getAssociatedClass()
+	{
+		return Response::class;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getSerializedValue()
+	{
+		return 'new Response()';
+	}
+}
