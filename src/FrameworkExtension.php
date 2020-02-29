62f4c1ebad8d13537d221f93dde52718f3de941d
plvhx <gandung@slackerc0de.us>
2020-02-29 17:24:55 +0700

    Import all compiler passes into use class list.

--- src/FrameworkExtension.php
+++ src/FrameworkExtension.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace LilleBitte\Extension\FrameworkExtension;
 
 use LilleBitte\Container\ContainerBuilderInterface;
+use LilleBitte\Extension\FrameworkExtension\Pass\EmitterPass;
+use LilleBitte\Extension\FrameworkExtension\Pass\RequestPass;
+use LilleBitte\Extension\FrameworkExtension\Pass\ResponsePass;
+use LilleBitte\Extension\FrameworkExtension\Pass\RouterFactoryPass;
 use LilleBitte\Kernel\Extension\Extension;
+use LilleBitte\Routing\Annotation\Route;
+use LilleBitte\Routing\Annotation\Method;
 
 /**
  * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
